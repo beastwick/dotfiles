e882662a0841bbdbe6cf328c85bdef57aa93e426
brian <brian>
2022-04-28 11:31:47 -0400

    some cleanup

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,47 +9,16 @@
 
 (package-initialize)
 
-(add-to-list 'load-path "~/gits/evil" "~/.emacs.d/")
-
-(require 'evil)
-
 (evil-mode 1)
-
-;; (setq org-directory "~/dropbox/org")
-;; (setq org-default-notes-file (concat org-directory "/*.org"))
-;; (global-set-key (kbd "C-c l") 'org-store-link)
-;; (global-set-key (kbd "C-c a") 'org-agenda)
-;; (global-set-key (kbd "C-c c") 'org-capture)
 
 (global-auto-revert-mode 1)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(projectile helm evil)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-level-1 ((t (:inherit outline-1 :foreground "brightcyan")))))
-
-(setq org-agenda-files '("~/dropbox/org/"))
+(setq org-agenda-files '("~/org/"))
 (setq org-refile-targets (quote ((org-agenda-files :maxlevel . 3))))
 (setq org-refile-use-outline-path 'file)
 (setq org-outline-path-complete-in-steps nil)
 (setq org-refile-allow-creating-parent-nodes 'confirm)
 
-(setq create-lockfiles nil)
-;disable backup
-(setq backup-inhibited t)
-;disable auto save
-(setq auto-save-default nil)
-
-(projectile-mode +1)
-(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-
-(require 'color-theme)
+;; (setq create-lockfiles nil)
+;; (setq backup-inhibited t)
+;; (setq auto-save-default nil)
